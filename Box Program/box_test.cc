d40fa2ad35fead310b20e422324528ba249bb7b6
99002530 <71605288+99002530@users.noreply.github.com>
2020-10-01 09:40:34 +0530

    Add files via upload

---
+++ Box Program/box_test.cc
@@ -0,0 +1,56 @@
+#include "box.cc"
+#include <gtest/gtest.h>
+
+TEST(Box,DefaultConstructor) {
+    Box a1;
+    EXPECT_EQ(0,a1.getlength());
+    EXPECT_EQ(0,a1.getbreadth());
+    EXPECT_EQ(0,a1.getheight());
+     EXPECT_EQ(0,a1.volume());
+
+}
+TEST(Box,ParameterizedConstructor) {
+    Box a1(10,20,30);
+
+    EXPECT_EQ(10,a1.getlength());
+    EXPECT_EQ(20,a1.getbreadth());
+
+    EXPECT_EQ(30,a1.getheight());
+
+
+}
+TEST(Box,CopyConstructor) {
+    Box a1(10,20,30);
+    Box a2(a1);
+    EXPECT_EQ(10,a2.getlength());
+
+    EXPECT_EQ(20,a2.getbreadth());
+    EXPECT_EQ(30,a2.getheight());
+
+
+}
+TEST(Box,VolumeTest) {
+    Box a1(10,20,30);
+
+    EXPECT_EQ(6000.0,a1.volume());
+
+}
+
+
+TEST(Box,DisplayTest) {
+    Box a1(10,20,30);
+    std::string ExpectedOut="10,20,30\n";
+    testing::internal::CaptureStdout();
+    a1.display();
+    std::string ActualOut = testing::internal::GetCapturedStdout();
+    EXPECT_STREQ(ExpectedOut.c_str(), ActualOut.c_str());
+}
+
+int main(int argc, char **argv) 
+    {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    }
+
+
+
